4d1eb6ef7b6bef8dcfb2250006b2425f586165ae
paschyz <paschy2002@gmail.com>
2023-07-29 06:01:58 +0200

    created CarouselTabs and TopPicks components

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -4,47 +4,13 @@ import { connect } from "react-redux";
 import { setMangas, setCurrentIndex } from "../reducers"; // Import your action creators from the reducers.ts file
 import { CarouselProps } from "../models/CarouselProps";
 import { CarouselState } from "../models/CarouselState.interface";
-
-function Feed({
-  mangas,
-  currentIndex,
-  setMangas,
-  setCurrentIndex,
-}: CarouselProps) {
-  useEffect(() => {
-    setMangas(mangass);
-  }, []);
-
-  const goToIndex = (key: number) => {
-    setCurrentIndex(key);
-    console.log(currentIndex);
-  };
-
+import TopPicks from "./TopPicks";
+import CarouselTabs from "./CarouselTabs";
+export default function Feed() {
   return (
     <div className="bg-black h-screen flex justify-center  ">
-      <div className="flex h-20 space-x-4 items-end">
-        {mangas.map((manga, index) => {
-          return (
-            <button
-              onClick={() => goToIndex(index)}
-              key={index}
-              className="bg-[#23252b] hover:bg-[#393B41] h-14 w-44 text-white brightness-105"
-            >
-              {manga.title}
-            </button>
-          );
-        })}
-      </div>
+      <CarouselTabs></CarouselTabs>
+      {/* <TopPicks></TopPicks> */}
     </div>
   );
 }
-
-// Map the state from the Redux store to props
-const mapStateToProps = (state: CarouselState) => {
-  return {
-    mangas: state.mangas,
-    currentIndex: state.currentIndex,
-  };
-};
-// Connect the component to the Redux store
-export default connect(mapStateToProps, { setMangas, setCurrentIndex })(Feed);
---
+++ src/components/TopPicks.tsx
@@ -0,0 +1,7 @@
+export default function TopPicks() {
+  return (
+    <div>
+      <p className="text-white">TopPicks</p>
+    </div>
+  );
+}
