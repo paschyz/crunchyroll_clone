63efdc1721f39ae3b24e21fa63f789083a60153a
paschyz <paschy2002@gmail.com>
2023-07-29 04:42:43 +0200

    buttons to display manga names

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavLink";
 import Carousel from "../components/Carousel";
+import Feed from "../components/Feed";
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
       <Carousel></Carousel>
+      <Feed></Feed>
     </div>
   );
 }
